261ab4d784b8c60975f001a4fe43face6977a08e
ruizaj13 <ruizaj13@gmail.com>
2020-11-06 09:13:00 -0800

    set state, starting on character component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [charData, setCharData] = useState()
+
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios
+    .get('https://rickandmortyapi.com/api/character/1,2,3,4,5')
+    .then((res => {
+      setCharData(data.res.results);
+    }))
+    .catch( err => {
+      console.log("Someone's not getting schwifty!")
+    })
+  })
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <div className='charHolder'>
+        </>
+      </div>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,29 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Character = props => {
+
+    const { data } = props
+    return(
+        <div>
+            {data.map((character) => {
+                return(
+                    <div>
+                        <li>Name: {character.name}</li>
+                        <li>Status: {character.status}</li>
+                        <li>Species: {character.species}</li>
+                        <li>Gender: {character.gender}</li>
+                    </div>
+                )
+            })}
+
+
+        </div>
+
+
+
+    )
+
+}
